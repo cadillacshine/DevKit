f95f8816420c0263d6faa2ff06420177a1f8b5c2
Boat <a.boat@hotmail.com>
2018-01-08 08:37:53 +0000

    Initial Commit

---
+++ DevKit/DevKit.h
@@ -0,0 +1,19 @@
+//
+//  DevKit.h
+//  DevKit
+//
+//  Created by DevFive on 1/8/18.
+//  Copyright © 2018 sulvsystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DevKit.
+FOUNDATION_EXPORT double DevKitVersionNumber;
+
+//! Project version string for DevKit.
+FOUNDATION_EXPORT const unsigned char DevKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevKit/PublicHeader.h>
+
+
